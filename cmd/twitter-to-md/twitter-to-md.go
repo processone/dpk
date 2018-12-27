041b08bdf639c8cd19924524a3308e7c2b2429b3
Mickael Remond <mremond@process-one.net>
2018-12-27 11:05:55 +0100

    Describe tool goal

--- cmd/twitter-to-md/twitter-to-md.go
+++ cmd/twitter-to-md/twitter-to-md.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/processone/dpk"
 
+// This tool is used to convert data from your Twitter archive to a set of Markdown files.
+// You can request your data from Twitter at this URL: https://twitter.com/settings/your_twitter_data
 func main() {
 	dpk.TwitterToMD("", "")
 }
