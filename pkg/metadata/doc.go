856cb361d1b2a2504c6ad278d0a2cadfe26f4c52
Mickael Remond <mremond@process-one.net>
2019-01-03 15:54:17 +0100

    Improves code documentation

--- pkg/metadata/doc.go
+++ pkg/metadata/doc.go
@@ -3,10 +3,10 @@ Metadata package is a set of tools to parse HTML pages to extract their metadata
 
 For now, the following set of metadata specifications are supported:
 
-- HTML 5 + RDFa
-- Dublin Core. Dublin Core metadata are defined here: http://www.dublincore.org/documents/dces/
-- Open Graph. Open Graph metadata (Facebook) are defined here: http://ogp.me/
-- Twitter. Twitter metadata are defined here: https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup
+	- HTML 5 + RDFa
+	- Dublin Core. Dublin Core metadata are defined here: http://www.dublincore.org/documents/dces/
+	- Open Graph. Open Graph metadata (Facebook) are defined here: http://ogp.me/
+	- Twitter. Twitter metadata are defined here: https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup
 
 */
 package metadata
